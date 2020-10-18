cd2ccee85cef98ca4910f724c54673ac6e83e2b7
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-10-18 17:21:45 +0200

    Ejercicio para motrar los primeros 100 numeros con bucle while

---
+++ ejercicioPropio02/src/ejerciciopropio02/EjercicioPropio02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopropio02;
+
+import static javafx.beans.binding.Bindings.and;
+
+/**
+ *
+ * @author angel
+ */
+public class EjercicioPropio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero =0;
+             while ( numero >=0 && numero <= 100){
+                    System.out.println(numero);
+        
+                    numero ++;
+    }
+    }
+    
+}
