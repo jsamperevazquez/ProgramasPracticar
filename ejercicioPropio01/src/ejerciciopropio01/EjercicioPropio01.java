bfc3db661648466be3762347a131535d09f0c736
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-10-18 17:20:51 +0200

    Ejercicio para motrar cual de dos numeros es mayor con uso de if y else

---
+++ ejercicioPropio01/src/ejerciciopropio01/EjercicioPropio01.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopropio01;
+
+/**
+ *
+ * @author angel
+ */
+public class EjercicioPropio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int numero1 =7;
+    int numero2 = 7;
+    
+    if (numero1<numero2)
+                {
+                    System.out.println("El número " + numero2 + " es mayor que el " + numero1);}
+                else if(numero1>numero2){
+                    System.out.println("El número " + numero1 + " es mayor que el " + numero2);
+                }
+                else {
+                    System.out.println("Los números son iguales");
+                }
+    }
+    
+}
