0fba1324c45de3c4bedc8097de63fc1a30213c5f
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-10-31 13:42:30 +0100

    Terminado boletín sobre eucaciones de segundo grado (Clases)

--- boletinPropioClases_02/Main.java
+++ boletinPropioClases_02/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
     Raices operacion1 = new Raices ();
-    operacion1.obtenerRaices();
-    operacion1.getDiscriminante();
+    operacion1.calcular();
     }
 }
--- boletinPropioClases_02/Raices.java
+++ boletinPropioClases_02/Raices.java
@@ -39,15 +39,9 @@ public class Raices {
         // Hallo las Raices posibles de la parábola.
         double raiz1= (-coeficienteB + Math.sqrt(getDiscriminante()))/(2*coeficienteA);
         double raiz2=(-coeficienteB - Math.sqrt(getDiscriminante()))/(2*coeficienteA);
-        if (getDiscriminante()>0) {
             System.out.println("El resultado de la ecuación de segundo grado es de:\n Raiz1= " + raiz1 + "\n Raiz2= " + raiz2);
-        }
-        else if (getDiscriminante()<0){
-            System.out.println("El discriminante es menor a 0 y todos sabemos que una ecuanción de números negativos no se expresa en R");
-        }
-        else System.out.println("La ecuación solo tiene una solución, y el resultado es: \nraiz1= " + raiz1 + "\n raiz2= "+ raiz2);
-
     }
+
 
     // Creo el método para devolver el valor del discriminante el valor del discriminante
     public double getDiscriminante(){
@@ -56,8 +50,28 @@ public class Raices {
         return discriminante;
     }
 
+    //Creo un método que me devuelva si tiene o no 2 raices
 
+    public boolean tieneRaices(){
+        return getDiscriminante()>0;
+        }
+    //Creo otro método para devolver una raiz
+    public  boolean tieneRaiz(){
+        return getDiscriminante()==0;
+    }
 
-
+    //Creo un método calcular para solucciones
+    public void calcular(){
+        if (tieneRaices()){
+            obtenerRaices();
+        }
+    else if (tieneRaiz()){
+            System.out.printf("La ecuación solo tiene una posible solucción y es: " );
+            obtenerRaices();
+        }
+    else System.out.printf("La ecuación no tiene solucción porque el discriminante es menor de 0 y todos sabemos que la rair de un numero negatiovo no existe en R");
+    }
 
 }
+
+
