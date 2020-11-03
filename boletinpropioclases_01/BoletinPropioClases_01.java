d3550ac39a6bc86720f812581125285f67108656
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-11-03 16:07:36 +0100

    Añadido formato para cumplir con convenio de Java

--- boletinpropioclases_01/BoletinPropioClases_01.java
+++ boletinpropioclases_01/BoletinPropioClases_01.java
@@ -6,7 +6,6 @@
 package boletinpropioclases_01;
 
 /**
- *
  * @author angel
  */
 public class BoletinPropioClases_01 {
@@ -15,37 +14,37 @@ public class BoletinPropioClases_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    //Instancio un nuevo objeto (cliente1) dentro de la clase Cuenta    
-        Cuenta cliente1 = new Cuenta ("Jose Ángel Sampere", 3000);
-        
-    // LLamo al metodo Get para poder ver el titular de la cuenta 
-       String verTitular= cliente1.getTitular();
-       System.out.println("Titular cuenta: " + verTitular);
-       
-    // LLamo al método ingresar para meter dinero (recordamos que si meto una cantidad negativa salta error)   
-       cliente1.ingresar(320);
-       
-    // LLamo al método Get para poder visualizar la cantidad.  
-       System.out.println("El nuevo saldo de la cuenta es de: " + cliente1.getCantidad());
 
-       
-    // Retiro dinero de cuenta con el método retirar() y llamo al método Get para comprobar
-    
+        //Instancio un nuevo objeto (cliente1) dentro de la clase Cuenta
+        Cuenta cliente1 = new Cuenta("Jose Ángel Sampere", 3000);
+
+        // LLamo al metodo Get para poder ver el titular de la cuenta
+        String verTitular = cliente1.getTitular();
+        System.out.println("Titular cuenta: " + verTitular);
+
+        // LLamo al método ingresar para meter dinero (recordamos que si meto una cantidad negativa salta error)
+        cliente1.ingresar(320);
+
+        // LLamo al método Get para poder visualizar la cantidad.
+        System.out.println("El nuevo saldo de la cuenta es de: " + cliente1.getCantidad());
+
+
+        // Retiro dinero de cuenta con el método retirar() y llamo al método Get para comprobar
+
         cliente1.retirar(500);
         System.out.println("El nuevo saldo de la cuenta es de: " + cliente1.getCantidad());
-    
-    //Pruebo a ver si deja la cuenta a 0 si retiro más dinero de lo que hay.
+
+        //Pruebo a ver si deja la cuenta a 0 si retiro más dinero de lo que hay.
         cliente1.retirar(5000);
         System.out.println("El nuevo saldo de la cuenta es de: " + cliente1.getCantidad());
-    
+
         //Instancio otro objeto en la clase Cuenta con un solo atributo.
-        
-        Cuenta cliente2 = new Cuenta ("Jose María Aznar");
-        
+
+        Cuenta cliente2 = new Cuenta("Jose María Aznar");
+
         //visualizo y compruebo que solo tiene un atributo porque he creado otra clase que permite meter solo un parámetro.
-        String verTitular2= cliente2.getTitular();
+        String verTitular2 = cliente2.getTitular();
         System.out.println("Titular cuenta: " + verTitular2);
-    
+
     }
 }
