cb4e95dbc786819a5468292cc6232a232e0f13c4
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2021-04-09 09:53:12 +0200

    Pull Request de Angel (#3)
    * Commit para pull request
    
    * Clase para practicar Pull Request
    
    Co-authored-by: José Ramón Aira Gil <jairagil@danielcastelao.org>

---
+++ practicarPullRequest/PullR.java
@@ -0,0 +1,12 @@
+package practicarPullRequest;
+
+/**
+ * Creado por @autor: angel
+ * El  09 de abr. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class PullR {
+    public static void main(String[] args) {
+        System.out.println("Esto es una práctica de pull Request");
+    }
+}
